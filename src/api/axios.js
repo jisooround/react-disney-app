ffdfe4959bb524401eb5ac77ad95c41f3f252a3b
jisooround <9utesleben@gmail.com>
2023-01-02 22:03:57 +0900

    https

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // 자주 사용하는 값을 instance로 저장
 const instance = axios.create({
-  baseURL: "http://api.themoviedb.org/3",
+  baseURL: "https://api.themoviedb.org/3",
   params: {
     api_key: "b18e798ff377ef49f1c335283e7c43d6",
     language: "ko-KR",
